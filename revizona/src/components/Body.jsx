15555908c5e1bbf0175e85aeffacd4c272c6c40a
josehvinii <jvictordauzaker@gmail.com>
2025-02-06 17:17:32 -0300

    Primeira aula da revisão

---
+++ revizona/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import './Body.css'
+import Demo1 from './Demo1'
+
+function Body() {
+  return (
+    <div className='container-Body'>
+        <h1>Empresa de logos</h1>
+        <Demo1 />
+    </div>
+  )
+}
+
+export default Body
---
+++ revizona/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css'
+function Header() {
+  return (
+    <div className="container-header">
+        <p>LoGo</p>
+        <p>Link1</p>
+        <p>Link2</p>
+        <p>Link5</p>
+    </div>
+  )
+}
+
+export default Header
